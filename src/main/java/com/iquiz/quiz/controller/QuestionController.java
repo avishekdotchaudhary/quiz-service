0d15d33987aa321ea3ec75d0be44795eb5274f93
avishe-yco <avishek.chaudhary@yco.com.np>
2025-05-15 14:41:57 +0545

    question service

---
+++ src/main/java/com/iquiz/quiz/controller/QuestionController.java
@@ -0,0 +1,32 @@
+package com.iquiz.quiz.controller;
+
+
+import com.iquiz.quiz.model.Question;
+import com.iquiz.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+}
---
+++ src/main/java/com/iquiz/quiz/dao/QuestionDao.java
@@ -0,0 +1,13 @@
+package com.iquiz.quiz.dao;
+
+import com.iquiz.quiz.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+}
